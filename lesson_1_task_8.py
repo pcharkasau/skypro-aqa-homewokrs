b15e07fe350a1b3ae2f4e245b61ee105c3e0f9d0
pcharkasau <whitevoyager2010@gmail.com>
2023-06-02 15:34:51 +0300

    Задание 8. Параметризация функций

---
+++ lesson_1_task_8.py
@@ -0,0 +1,17 @@
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+def some_function(num):
+    print(num[7])
+    print(num[7])
+    print(num[9])
+    print(num[9])
+    print(num[9])
+    print(num[4])
+    print(num[4])
+    print(num[4])
+    print(num[2])
+    print(num[4])
+    print(num[2])
+    print(num[4])
+
+some_function(num)
