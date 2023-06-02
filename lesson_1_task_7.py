1e26556724a5f6c4cdbcff124ea3aa4d15a9b642
pcharkasau <whitevoyager2010@gmail.com>
2023-06-02 15:34:27 +0300

    Задание 7. Вызов функций

---
+++ lesson_1_task_7.py
@@ -0,0 +1,41 @@
+def numberA():
+    inConsol = 1
+    print(inConsol)   # Вывести в консоль 1
+def numberB():
+    inConsol = 2
+    print(inConsol)   # Вывести в консоль 2
+def numberC():
+    inConsol = 3
+    print(inConsol)   # Вывести в консоль 3
+def numberD():
+    inConsol = 4
+    print(inConsol)   # Вывести в консоль 4
+def numberE():
+    inConsol = 5
+    print(inConsol)   # Вывести в консоль 5
+def numberF():
+    inConsol = 6
+    print(inConsol)   # Вывести в консоль 6
+def numberG():
+    inConsol = 7
+    print(inConsol)   # Вывести в консоль 7
+def numberH():
+    inConsol = 8
+    print(inConsol)   # Вывести в консоль 8
+def numberI():
+    inConsol = 9
+    print(inConsol)   # Вывести в консоль 9
+def numberJ():
+    inConsol = 0
+    print(inConsol)   # Вывести в консоль 0
+numberH()   # вызов функции H
+numberH()   # вызов функции H
+numberJ()   # вызов функции J
+numberJ()   # вызов функции J
+numberE()   # вызов функции E
+numberE()   # вызов функции E
+numberE()   # вызов функции E
+numberC()   # вызов функции C
+numberE()   # вызов функции E
+numberC()   # вызов функции C
+numberE()   # вызов функции E
