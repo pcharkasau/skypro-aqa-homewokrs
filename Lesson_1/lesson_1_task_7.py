e365b1d828f2395afdb2038b9a965f9e73593d3f
pcharkasau <whitevoyager2010@gmail.com>
2023-06-03 13:15:57 +0300

    Добавлен альтернативное решение от наставника

--- Lesson_1/lesson_1_task_7.py
+++ Lesson_1/lesson_1_task_7.py
@@ -39,3 +39,36 @@ numberC()   # вызов функции C
 numberE()   # вызов функции E
 numberC()   # вызов функции C
 numberE()   # вызов функции E
+
+# альтернативный вариант решения от наставника
+def number_a():
+    print(1, end = '')   # Вывести в консоль 1
+def number_b():
+    print(2, end = '')   # Вывести в консоль 2
+def number_c():
+    print(3, end = '')   # Вывести в консоль 3
+def number_d():
+    print(4, end = '')   # Вывести в консоль 4
+def number_e():
+    print(5, end = '')   # Вывести в консоль 5
+def number_f():
+    print(6, end = '')   # Вывести в консоль 6
+def number_g():
+    print(7, end = '')   # Вывести в консоль 7
+def number_h():
+    print(8, end = '')   # Вывести в консоль 8
+def number_i():
+    print(9, end = '')   # Вывести в консоль 9
+def number_j():
+    print(0, end = '')   # Вывести в консоль 0
+number_h()   # вызов функции h
+number_h()   # вызов функции h
+number_j()   # вызов функции j
+number_j()   # вызов функции j
+number_e()   # вызов функции e
+number_e()   # вызов функции e
+number_e()   # вызов функции e
+number_c()   # вызов функции c
+number_e()   # вызов функции e
+number_c()   # вызов функции c
+number_e()   # вызов функции e
