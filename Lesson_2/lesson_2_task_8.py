721a41b9953de9469b74a3928cc136325a35c761
pcharkasau <whitevoyager2010@gmail.com>
2023-06-12 13:14:06 +0300

    Исправлено задание 8

--- Lesson_2/lesson_2_task_8.py
+++ Lesson_2/lesson_2_task_8.py
@@ -4,9 +4,6 @@
 # 3. Создайте список [ 18, 14, 10, 6, 2 ] с помощью функции range() и выведите его на экран.
 
 
-lst = [ 18, 14, 10, 6, 2 ]
-
-for x in range (0, len(lst)):
-    print(lst[x])
-
-
+import numpy as np
+arr = np.arange(18, 1, -4)
+print(arr)
